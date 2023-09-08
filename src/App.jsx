6d21d78eab4622b97e129c30c769002f7d854839
Enrique Padron Spångberg <enrique.padron853@gmail.com>
2023-09-08 09:33:05 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import TaskList from "./components/Tasklist";
+import { TaskForm } from "./components/TaskForm";
+
+import "./App.css";
+
+function App() {
+  return (
+    <main className="bg-zinc-900 h-screen">
+      <div className="container mx-auto p-10">
+        <TaskForm />
+        <TaskList />
+      </div>
+    </main>
+  );
+}
+
+export default App;
